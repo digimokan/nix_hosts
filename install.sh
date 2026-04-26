f9ff23f82b458a528280bfab9e600d0054a03c80
digimokan <digimokan@users.noreply.github.com>
2026-04-26 12:23:24 -0500

    Fixup write-efi-boot-entries string

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ nix --extra-experimental-features "nix-command flakes" \
   run "github:nix-community/disko#disko-install" -- \
   --flake ".#nas-0" \
   --disk main "/dev/disk/by-id/nvme-KINGSTON_SNV3S500G_50026B76876DA41F" \
-  "$([ -d /sys/firmware/efi ] && echo "--write-efi-boot-entries")"
+  "$([ -d /sys/firmware/efi ] && echo '--write-efi-boot-entries')"
 
