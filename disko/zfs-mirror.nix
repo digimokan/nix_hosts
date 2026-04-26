d6925eaed79987e293c24b6fe5b237a1ad81c7c4
digimokan <digimokan@users.noreply.github.com>
2026-04-26 12:10:51 -0500

    Restore all, don't spec "devices" in disko cfg at all

--- disko/zfs-mirror.nix
+++ disko/zfs-mirror.nix
@@ -1,82 +1,80 @@
-# { ... }: {
-#   disko.devices = {
-#     disk = {
-#       main = {
-#         type = "disk";
-#         device = builtins.elemAt disksSel 0;
-#         content = {
-#           type = "gpt";
-#           partitions = {
-#             bios_boot = {
-#               size = "1M";
-#               type = "EF02";
-#             };
-#             ESP = {
-#               size = "512M";
-#               type = "EF00";
-#               content = {
-#                 type = "filesystem";
-#                 format = "vfat";
-#                 mountpoint = "/boot";
-#               };
-#             };
-#             zfs = {
-#               size = "100%";
-#               content = {
-#                 type = "zfs";
-#                 pool = "zroot";
-#               };
-#             };
-#           };
-#         };
-#       };
-#       secondary = {
-#         type = "disk";
-#         device = builtins.elemAt disksSel 1;
-#         content = {
-#           type = "gpt";
-#           partitions = {
-#             bios_boot = {
-#               size = "1M";
-#               type = "EF02";
-#             };
-#             ESP = {
-#               size = "512M";
-#               type = "EF00";
-#               content = {
-#                 type = "filesystem";
-#                 format = "vfat";
-#                 mountpoint = "/boot-fallback";
-#               };
-#             };
-#             zfs = {
-#               size = "100%";
-#               content = {
-#                 type = "zfs";
-#                 pool = "zroot";
-#               };
-#             };
-#           };
-#         };
-#       };
-#     };
-#     zpool.zroot = {
-#       type = "zpool";
-#       mode = "mirror";
-#       mountpoint = "/";
-#       rootFsOptions = {
-#         compression = "lz4";
-#         acltype = "posixacl";
-#         xattr = "sa";
-#         atime = "off";
-#       };
-#       datasets = {
-#         "var" = {
-#           type = "zfs_fs";
-#           mountpoint = "/var";
-#         };
-#       };
-#     };
-#   };
-# }
-#
+{ ... }: {
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            bios_boot = {
+              size = "1M";
+              type = "EF02";
+            };
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "zroot";
+              };
+            };
+          };
+        };
+      };
+      secondary = {
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            bios_boot = {
+              size = "1M";
+              type = "EF02";
+            };
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot-fallback";
+              };
+            };
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "zroot";
+              };
+            };
+          };
+        };
+      };
+    };
+    zpool.zroot = {
+      type = "zpool";
+      mode = "mirror";
+      mountpoint = "/";
+      rootFsOptions = {
+        compression = "lz4";
+        acltype = "posixacl";
+        xattr = "sa";
+        atime = "off";
+      };
+      datasets = {
+        "var" = {
+          type = "zfs_fs";
+          mountpoint = "/var";
+        };
+      };
+    };
+  };
+}
+
--- disko/zfs-single-disk.nix
+++ disko/zfs-single-disk.nix
@@ -1,52 +1,51 @@
-# { ... }: {
-#   disko.devices = {
-#     disk = {
-#       main = {
-#         type = "disk";
-#         device = "/dev/disk/by-id/nvme-KINGSTON_SNV3S500G_50026B76876DA41F";
-#         content = {
-#           type = "gpt";
-#           partitions = {
-#             bios_boot = {
-#               size = "1M";
-#               type = "EF02";
-#             };
-#             ESP = {
-#               size = "512M";
-#               type = "EF00";
-#               content = {
-#                 type = "filesystem";
-#                 format = "vfat";
-#                 mountpoint = "/boot";
-#               };
-#             };
-#             zfs = {
-#               size = "100%";
-#               content = {
-#                 type = "zfs";
-#                 pool = "zroot";
-#               };
-#             };
-#           };
-#         };
-#       };
-#     };
-#     zpool.zroot = {
-#       type = "zpool";
-#       mountpoint = "/";
-#       rootFsOptions = {
-#         compression = "lz4";
-#         acltype = "posixacl";
-#         xattr = "sa";
-#         atime = "off";
-#       };
-#       datasets = {
-#         "var" = {
-#           type = "zfs_fs";
-#           mountpoint = "/var";
-#         };
-#       };
-#     };
-#   };
-# }
-#
+{ ... }: {
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            bios_boot = {
+              size = "1M";
+              type = "EF02";
+            };
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "zroot";
+              };
+            };
+          };
+        };
+      };
+    };
+    zpool.zroot = {
+      type = "zpool";
+      mountpoint = "/";
+      rootFsOptions = {
+        compression = "lz4";
+        acltype = "posixacl";
+        xattr = "sa";
+        atime = "off";
+      };
+      datasets = {
+        "var" = {
+          type = "zfs_fs";
+          mountpoint = "/var";
+        };
+      };
+    };
+  };
+}
+
