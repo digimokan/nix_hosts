063e783219dcc8c8b909382de839296483822f4b
digimokan <digimokan@users.noreply.github.com>
2026-04-25 21:06:52 -0500

    config.nix: enable cmd & flakes, cond GRUB/sysdboot

--- hosts/nas/configuration.nix
+++ hosts/nas/configuration.nix
@@ -1,23 +1,31 @@
-{ pkgs, hostSel, ... }: {
+{ pkgs, hostSel, lib, ... }: {
   boot.supportedFilesystems = [ "zfs" ];
   networking.hostName = hostSel.hostNameSel;
   networking.hostId = hostSel.hostIdSel;
 
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  boot.loader.systemd-boot.enable = hostSel.isUefiSel;
+  boot.loader.efi.canTouchEfiVariables = hostSel.isUefiSel;
+
+  boot.loader.grub = lib.mkIf (!hostSel.isUefiSel) {
+    enable = true;
+    zfsSupport = true;
+    devices = hostSel.rootPoolDisksSel;
+  };
+
   fileSystems."/data" = {
     device = "zdata";
     fsType = "zfs";
-    options = [ "nofail" "canmount=on" ];
-  };
-
-  boot.loader.grub = {
-    enable = true;
-    zfsSupport = true;
-    efiSupport = hostSel.isUefiSel;
-    devices = hostSel.rootPoolDisksSel;
-    copyKernels = true;
+    options = [
+      "nofail"
+      "canmount=on"
+    ];
   };
 
   users.users.root.initialPassword = "nixos";
   system.stateVersion = "24.11";
 }
-
