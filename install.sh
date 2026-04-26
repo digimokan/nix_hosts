bb3b3edc1d99311365a1acd907464e0f92757ecc
digimokan <digimokan@users.noreply.github.com>
2026-04-26 12:21:36 -0500

    Add deployment cmd as install.sh script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+nix --extra-experimental-features "nix-command flakes" \
+  run "github:nix-community/disko#disko-install" -- \
+  --flake ".#nas-0" \
+  --disk main "/dev/disk/by-id/nvme-KINGSTON_SNV3S500G_50026B76876DA41F" \
+  "$([ -d /sys/firmware/efi ] && echo "--write-efi-boot-entries")"
+
