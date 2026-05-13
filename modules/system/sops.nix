e2a668a2fe089a4d438834e1dee627df79c07bcc
digimokan <digimokan@users.noreply.github.com>
2026-05-13 11:44:23 -0500

    Make sops use sops pkg vers from its flake input

--- modules/system/sops.nix
+++ modules/system/sops.nix
@@ -1,8 +1,8 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, inputs, ... }:
 
 let
 
-cfg = config.custom.system.sops;
+  cfg = config.custom.system.sops;
 
 in {
 
@@ -16,13 +16,16 @@ in {
 
   config = lib.mkIf cfg.enable {
     # By default, use the secrets accessible to all hosts.
-    sops.defaultSopsFile = ../../secrets/shared_all_hosts_secrets.yaml.yaml;
+    sops.defaultSopsFile = ../../secrets/shared_all_hosts_secrets.yaml;
     sops.defaultSopsFormat = "yaml";
 
     # Tell the NixOS host exactly where to look for its private key.
     # Host will use the private key to decrypt the secrets.yaml file during the
     # boot sequence.
     sops.age.keyFile = "/var/lib/sops-nix/host_keypair.age";
+
+    # Override the default package to use the pre-built version from the flake input
+    sops.package = inputs.sops-nix.packages.${pkgs.stdenv.hostPlatform.system}.sops-install-secrets;
   };
 
 }
