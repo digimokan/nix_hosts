ac66e6c8882e97efbf6ea0875e8a5a1eed6f930b
digimokan <digimokan@users.noreply.github.com>
2026-08-29 17:21:28 -0500

    tm2: test out cosmic panelPosition "Top"

--- hosts/tm2/default.nix
+++ hosts/tm2/default.nix
@@ -59,7 +59,8 @@ in {
 
     custom.apps.cosmic.enableDisplayMgr = true;
     custom.apps.cosmic.enableDesktopEnv = true;
-    custom.apps.cosmic.users.testuser2 = {};
+    # custom.apps.cosmic.users.testuser2 = {};
+    custom.apps.cosmic.users.testuser2.panelPosition = "Top";
 
     custom.apps.pipewire.enable = true;
 
