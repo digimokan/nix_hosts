9bb09c87d54e1a4194ef6c83b790eb39d12f8f59
digimokan <digimokan@users.noreply.github.com>
2026-09-07 08:27:07 -0500

    Disable cosmic auto-login to resolve screen-lock bug

--- hosts/common/user-desktop-reg.nix
+++ hosts/common/user-desktop-reg.nix
@@ -24,7 +24,6 @@ in {
     custom.system.wayland.enableXWayland = true;
 
     custom.apps.cosmic.enableDisplayMgr = true;
-    custom.apps.cosmic.autoLoginUser = infra.primaryUser;
     custom.apps.cosmic.enableDesktopEnvForUsers = infra.allUserNames;
 
     custom.apps.pipewire.enable = true;
